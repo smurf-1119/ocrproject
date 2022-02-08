462399e4536c2108fdc701d7b7336a0332c4fb64
RobertLuo1 <robertluo171@gmail.com>
2022-02-08 23:13:14 +0800

    the dataset

---
+++ recognitiondataset.py
@@ -0,0 +1,80 @@
+import torch
+from torch.utils.data import DataLoader,Dataset
+from utils.data_utilis import *
+import json
+import cv2
+import os
+import platform
+import matplotlib.pyplot as plt
+
+class recognitionDataset(Dataset):
+    def __init__(self,mode,input_dim,max_sequence) -> None:
+        super(recognitionDataset,self).__init__()
+        self.max_sequence = max_sequence
+        self.input_dim = input_dim
+        self.vocab_path = './dict.txt'
+        self.vocab = build_vocab(self.vocab_path)
+        if mode == 'train':
+            self.img_base_file = './AEC_recognition/train/img'
+            label_file = './AEC_recognition/train/labels.json'
+            with open(label_file,'r') as f:
+                self.labels = json.load(f)['labels']
+        elif mode =='validation':
+            self.img_base_file = './AEC_recognition/validation/img'
+            label_file = './AEC_recognition/validation/labels.json'
+            with open(label_file,'r') as f:
+                self.labels = json.load(f)['labels']
+    def __getitem__(self, index):
+        data = self.labels[index]
+        image_path = data['image_path']
+        image = np.array(cv2.imread(os.path.join(self.img_base_file,image_path)))
+        resize_image = prep_image(image,self.input_dim)
+        label = data['label']
+        label = process_label(self.max_sequence,self.vocab,label)
+        return resize_image,label
+    def __len__(self):
+        return len(self.labels) 
+
+def collate_fn(batch):
+    images = None
+    labels = []
+    for img, label in batch:
+        if images == None:
+            images = img
+        else:
+            images = torch.cat([images, img], 0)
+        labels.append(label)
+    return [images, labels]
+
+
+
+def getDataLoader(mode,batch_size,max_sequence):
+    """"
+    descriptions:generate the dataloader for the following operations
+    params:
+    @mode{str}:train,validation or test
+    @batch_size{int}:the variable for the dataset
+    @max_sequence{int}: make sure that the label is in a appropirate length
+    """
+    dataset = recognitionDataset(mode,416,max_sequence)
+    if mode == 'test':
+        iter = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers= 4 if platform.system() == "linux" else 0, collate_fn=collate_fn)
+    else:
+        iter = DataLoader(dataset,batch_size=batch_size,shuffle=True,num_workers= 4 if platform.system() == "linux" else 0,collate_fn=collate_fn)
+    return iter
+
+
+trainLoader = getDataLoader('train',2,7)
+for image,label in trainLoader:
+    print(image.size())
+    print(label)
+    break
+# dataset = recognitionDataset('train',224)
+# img = dataset[0][0]
+# label = dataset[0][1]
+# print(img.shape)
+# print(label)
+# plt.imshow(img)
+# plt.show()
+
+
--- utils/data_utilis.py
+++ utils/data_utilis.py
@@ -1,11 +1,10 @@
-'''
-help construct data
-'''
+"""
+help construct the dataeset
+"""
 
-from black import out
-import torchtext.vocab as Vocab
-import collections
 import torch
+import cv2
+import numpy as np
 
 PAD, BOS, EOS = '<pad>', '<bos>', '<eos>'
 
@@ -15,7 +14,7 @@ def process_label(max_seq_len: int, vocab: dict, label: str):
     params:
         @max_seq_len{int}: the max length of label seqence;
         @vocab{dict}: the dictionary to map the strings to integers;
-        @label{str}: 
+        @labels{list}: (batch, )
     return:
         out_label{tensor}
     '''
@@ -32,8 +31,6 @@ def process_label(max_seq_len: int, vocab: dict, label: str):
     
     return torch.tensor(out_label)
 
-
-
 def build_vocab(dict_name: str):
     PAD, BOS, EOS = '<pad>', '<bos>', '<eos>'
 
@@ -45,4 +42,25 @@ def build_vocab(dict_name: str):
 
     return vocab
 
+def letterbox_image(img, inp_dim:int):
+    '''resize image with unchanged aspect ratio using padding'''
+    img_w, img_h = img.shape[1], img.shape[0]
+    w, h = inp_dim
+    new_w = int(img_w * min(w/img_w, h/img_h))
+    new_h = int(img_h * min(w/img_w, h/img_h))
+    resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)   
+    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+    canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w, :] = resized_image
+    return canvas
+
+def prep_image(img, inp_dim):
+    """
+    Prepare image for inputting to the neural network. 
+    
+    Returns a Variable 
+    """
+    img = letterbox_image(img, (inp_dim, inp_dim))
+    img = img[:,:,::-1].transpose((2,0,1)).copy()
+    img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
+    return img
 
