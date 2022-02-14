586aecc280ce38a991f3088ae01ae25a9496c23a
smurf <1250782604@qq.com>
2022-02-14 21:54:46 +0800

    modify

--- ANMT.py
+++ ANMT.py
@@ -2,7 +2,6 @@
 recognition model
 '''
 
-from contextvars import Context
 from numpy import indices
 from zmq import device
 from utils.data_utilis import process_label, build_vocab
@@ -59,7 +58,7 @@ class ANMT(nn.Module):
                 mask = mask * (y != self.vocab[EOS]).float()
             # return dec_output
             return l / num_not_pad_tokens
-        else:
+        elif mode == 'validation':
             mask, num_not_pad_tokens = torch.ones((batch_size,), device=self.device), 0
             correct = torch.tensor([0.0],device=self.device)
             for y in Y.permute(1,0):
@@ -70,6 +69,8 @@ class ANMT(nn.Module):
                 num_not_pad_tokens +=mask.sum().item()
                 mask = mask * (y != self.vocab[EOS]).float()
             return correct/num_not_pad_tokens
+        else:
+            pass
                 
 def main():
     # params
